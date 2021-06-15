3c120a23db271598e42d5e1ebe0d62e9b0ad18d9
jh3618-kim <jh3618.kim@samsung.com>
2021-06-16 08:59:12 +0900

    yarn create strapi-app strapi

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8bbc78fffbc0136f07fc140741afdc0d'),
+    },
+  },
+});
