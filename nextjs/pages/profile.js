d57c127ed463992f766941043d2a0010216ad212
jh3618-kim <jh3618.kim@samsung.com>
2021-06-17 01:44:30 +0900

    use SWR Hook

--- nextjs/pages/profile.js
+++ nextjs/pages/profile.js
@@ -1,10 +1,27 @@
 import Router from 'next/router';
+import { useEffect } from 'react';
+import useUser from '../data/useUser';
+import { logout } from '../requests/userApi';
 
 function Profile() {
+  const { user, mutateUser, loading, loggedOut } = useUser();
+
+  useEffect(() => {
+    if (loggedOut) {
+      Router.replace('/login');
+    }
+  }, [loggedOut]);
+
+  if (loggedOut) return 'redirecting...';
+  if (loading) return 'loading...';
+
   return (
     <div>
+      <div>{user.email}</div>
       <button
-        onClick={async () => {
+        onClick={() => {
+          logout();
+          mutateUser();
           Router.push('/login');
         }}
       >
