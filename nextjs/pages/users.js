ac8a90e44d335303a480d4e64c84d7c642dc9bd6
jh3618-kim <jh3618.kim@samsung.com>
2021-06-16 13:23:41 +0900

    use getServerSideProps with fetch

--- nextjs/pages/users.js
+++ nextjs/pages/users.js
@@ -1,5 +1,12 @@
-function Users() {
-  return <div>This is a Users Page</div>;
+function Users({ users }) {
+  return <div>{JSON.stringify(users)}</div>;
+}
+
+export async function getServerSideProps() {
+  const users = await fetch('http://localhost:1337/users').then((res) => res.json());
+  return {
+    props: { users },
+  };
 }
 
 export default Users;
