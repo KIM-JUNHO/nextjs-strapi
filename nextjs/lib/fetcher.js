c18931320fb1c57ecd1e79e3ef5dbe5117e27f4b
jh3618-kim <jh3618.kim@samsung.com>
2021-06-17 00:28:12 +0900

    add graphql fetcher

---
+++ nextjs/lib/fetcher.js
@@ -0,0 +1,22 @@
+export async function fetcher(query, { variables } = {}) {
+  const endpoint = `${process.env.API_URL || 'http://localhost:1337'}/graphql`;
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  };
+  const res = await fetch(endpoint, options);
+
+  const json = await res.json();
+  if (json.errors) {
+    console.error(json.errors);
+    throw new Error('Failed to fetch API');
+  }
+
+  return json.data;
+}
