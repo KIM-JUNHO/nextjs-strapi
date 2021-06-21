6ec3c00dc76846836df82b623bf8d4fdcafdddc8
jh3618-kim <jh3618.kim@samsung.com>
2021-06-22 00:54:40 +0900

    Nextjs Dynamic Link

--- nextjs/components/RequestsTable.js
+++ nextjs/components/RequestsTable.js
@@ -5,6 +5,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Link from 'next/link';
 
 export default function RequestsTable({ requests }) {
   return (
@@ -23,7 +24,9 @@ export default function RequestsTable({ requests }) {
           {requests.map((row) => (
             <TableRow key={row.id}>
               <TableCell component="th" scope="row">
-                {row.id}
+                <Link href="/requests/[id]" as={`/requests/${row.id}`}>
+                  <a>{row.id}</a>
+                </Link>
               </TableCell>
               <TableCell>{row.created_at}</TableCell>
               <TableCell>{row.user.username}</TableCell>
