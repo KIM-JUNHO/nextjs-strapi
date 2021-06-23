36d2b2169e784d3beda1cbfe8575c271ea8d4244
jh3618-kim <jh3618.kim@samsung.com>
2021-06-23 17:45:37 +0900

    bug fix

--- nextjs/lib/fetch.js
+++ nextjs/lib/fetch.js
@@ -2,10 +2,15 @@ import { parseCookies } from 'nookies';
 
 export async function fetchWithToken(...args) {
   const { jwt } = parseCookies();
-  const res = await fetch(...args, {
+  const req = await fetch(...args, {
     headers: {
       Authorization: `Bearer ${jwt}`,
     },
   });
-  return res.json();
+  const res = await req.json();
+  if (res.error) {
+    console.error(res.error);
+    throw new Error('Failed to fetch API');
+  }
+  return res;
 }
